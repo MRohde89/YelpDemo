c78568c4040e00ee88553a9f6b6f4feccb31aecc
MRohde89 <rheinsberg1@gmx.net>
2016-05-16 15:45:51 +0200

    initial upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yelpdemo::Application.config.secret_key_base = '1a378d65b8483796b36f8f2bdd6c9a2f329143e83fa46fc54326178d2b905730bdd5dae36a42bb463d15eb23c494c4e03526e3556792a42b7680bb7251d55801'
