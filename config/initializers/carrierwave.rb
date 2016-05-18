2c17427d439473926c03be14c599d8899be4d613
MRohde89 <rheinsberg1@gmx.net>
2016-05-18 19:52:44 +0200

    image uploading problem fixed

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,9 @@
 CarrierWave.configure do |config|
+#  config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAJ57MGWQ3C2AP4BMA',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     'AKIAJACHV76CDIJOYS6Q',                        # required
+    aws_secret_access_key: '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',                        # required
   }
-  config.fog_directory  = 'baserailsyelpdemo'                          # required
+  config.fog_directory  = 'yelpdemotestbucket'                          # required
 end
